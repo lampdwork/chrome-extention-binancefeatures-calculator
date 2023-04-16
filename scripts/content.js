a0ec1c49753a3bde084642134f4cd8d6e8d7b873
lampd <lam@tgl-sol.com>
2023-04-16 14:43:02 +0700

    fix: set input value

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,5 @@
 const TAB_LOADING_TIMEOUT = 1000
-const POPUP_LOADING_TIMEOUT = 1000
+const POPUP_LOADING_TIMEOUT = 500
 const PAGE_LOADING = 5000
 const REFRESH_INTERVAL = 500
 
@@ -29,41 +29,36 @@ const calMarginAndSize = (entry, risk, riskPercent, stopLoss) => {
 const setMargin = (element, margin, marginRecommend, sizeInput, size) => {
   // Open popup
   element.click()
-  const marginInputContainer = document.querySelector('.leverage-container')
-  const marginInput = marginInputContainer.querySelector('input')
-  const marginRatio = document.querySelectorAll('.bn-slider-scale')
-  const maxMargin = parseInt(
-    marginRatio[marginRatio.length - 1].textContent.replace('x', '')
-  )
 
-  marginRecommend.innerHTML = `Recommend <span style="color: rgb(246, 70, 93); font-weight: 700;">${margin}</span>`
-  marginInputContainer.insertAdjacentElement('beforebegin', marginRecommend)
+  setTimeout(() => {
+    const marginInputContainer = document.querySelector('.leverage-container')
+    const marginInput = marginInputContainer.querySelector('input')
+    const marginRatio = document.querySelectorAll('.bn-slider-scale')
+    const confirmButton = document.querySelector('.footer > button')
+    const maxMargin = parseInt(
+      marginRatio[marginRatio.length - 1].textContent.replace('x', '')
+    )
 
-  marginInput.focus()
-  marginInput.addEventListener('blur', () => {
+    marginRecommend.innerHTML = `Recommend <span style="color: rgb(246, 70, 93); font-weight: 700;">${margin}</span>`
+    marginInputContainer.insertAdjacentElement('beforebegin', marginRecommend)
+
+    marginInput.value = margin
+    marginInput.dispatchEvent(new Event('change', { bubbles: true }))
+
     setTimeout(() => {
-      sizeInput.value = size
-    }, 1000)
-  })
+      if (maxMargin >= margin) {
+        confirmButton.click()
+      }
+    }, POPUP_LOADING_TIMEOUT)
+
+    confirmButton.addEventListener('click', () => {
+      setTimeout(() => {
+        sizeInput.value = size
+        sizeInput.dispatchEvent(new Event('change', { bubbles: true }))
+      }, 1000)
+    })
+  }, POPUP_LOADING_TIMEOUT)
 }
-
-// const setSize = (
-//   entry,
-//   stopLoss,
-//   risk,
-//   riskPercent,
-//   setMarginEl,
-//   marginRecommend
-// ) => {
-//   const { margin, size } = calMarginAndSize(entry, risk, riskPercent, stopLoss)
-
-//   sizeInput.value = size
-
-//   inputsOrder[4].addEventListener('blur', () => {
-//     setMargin(setMarginEl[1], margin, marginRecommend)
-//     console.log(margin, 'margin')
-//   })
-// }
 
 const listenOrderForm = (orderInputs) => {
   const { inputsOrder, activeTab, setMarginEl } = orderInputs
@@ -72,18 +67,20 @@ const listenOrderForm = (orderInputs) => {
   const marginRecommend = document.createElement('div')
   const sizeInput = inputsOrder[1]
 
-  let size = 0
-  let margin = 0
-
   if (activeTab.getAttribute('date-testid') === 'limit') {
     console.log('limit')
-    inputsOrder[4].addEventListener('input', (e) => {
+    // inputsOrder[4].addEventListener('input', (e) => {
+    //   sizeInput.value = size
+    //   sizeInput.dispatchEvent(new Event('change', { bubbles: true }))
+    // })
+
+    inputsOrder[4].addEventListener('blur', () => {
+      const entry = inputsOrder[0].value.replace(',', '.')
+      const stopLoss = inputsOrder[4].value.replace(',', '.')
       const risk = parseInt(document.querySelector('#max-risk')?.value) || 5
       const riskPercent =
         parseInt(document.querySelector('#max-risk-percent')?.value) / 100 ||
         0.5
-      const entry = inputsOrder[0].value.replace(',', '.')
-      const stopLoss = e.target.value.replace(',', '.')
 
       const { margin, size } = calMarginAndSize(
         entry,
@@ -92,12 +89,8 @@ const listenOrderForm = (orderInputs) => {
         stopLoss
       )
 
-      sizeInput.value = size
-
-      inputsOrder[4].addEventListener('blur', () => {
-        setMargin(setMarginEl[1], margin, marginRecommend, sizeInput, size)
-        console.log(margin, 'margin')
-      })
+      setMargin(setMarginEl[1], margin, marginRecommend, sizeInput, size)
+      console.log(margin, 'margin')
     })
   }
 }
@@ -113,7 +106,7 @@ const main = (orderForm) => {
 
     riskForm.innerHTML = `<div style="background: #800303; position: absolute; top: 0; left: 0; width: calc(100% - 20px); z-index: 1;flex-direction: column; padding: 10px">
       <div style="outline: none; color: white; display: flex; justify-content: space-between; margin-bottom: 5px;">Risk <input id="max-risk" style="width: 100px;" value="5"/></div>
-      <div style="outline: none; color: white; display: flex; justify-content: space-between;">Percent(%) <input id="max-risk-percent" style="width: 100px;" value="25"/></div>
+      <div style="outline: none; color: white; display: flex; justify-content: space-between;">Percent (%) <input id="max-risk-percent" style="width: 100px;" value="25"/></div>
     </div>`
 
     parentRiskForm.append(riskForm)
@@ -150,3 +143,7 @@ const interval = setInterval(() => {
     clearInterval(interval)
   }
 }, REFRESH_INTERVAL)
+
+setTimeout(() => {
+  clearInterval(interval)
+}, 60000)
